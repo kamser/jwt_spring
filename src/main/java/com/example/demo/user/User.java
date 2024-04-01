698c81abe7a8a9ec6f64f0546c73deb0dc18046f
kamser <keylor.morataya@gmail.com>
2024-03-31 20:45:15 -0700

    Added the UserDetails implementation to the User class to work with spring security and automatize the context update

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "_user")
@@ -12,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor //This next two annotation is for the cases when I have a constructor with no args and with all args.
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE) //This annotiation is the one than made posible to autogenerate the id with no need to pass it.
     private Integer id;
@@ -20,4 +24,34 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
