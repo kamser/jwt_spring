421b304c8b7301e2c019b66d2365e97daf71f999
kamser <keylor.morataya@gmail.com>
2024-03-31 21:06:15 -0700

    Created the first jwt filter whom trigger per request, it is the basic shape, need to complete the implementation for the method doFilterInternal()

---
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+//This class represents the first filter that triggers everytime a Request tries to get a resource from the server
+//so this class has to extends the class OncePerRequestFilter to have a listener when a request arrives to the server.
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
